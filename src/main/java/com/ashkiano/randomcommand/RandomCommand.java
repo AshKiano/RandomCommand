e6d48e52838068911d73b0660754f8836a0128ee
AshKiano <ash.kiano@gmail.com>
2023-06-22 23:04:05 +0200

    version 1.1

--- src/main/java/com/ashkiano/randomcommand/RandomCommand.java
+++ src/main/java/com/ashkiano/randomcommand/RandomCommand.java
@@ -7,14 +7,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import java.util.List;
 import java.util.Random;
-
+//TODO přidat hex podporu do další verze
+//TODO upravit zobrazování odpočtu a z minut zmenit na vteriny interval odpoctu
 public class RandomCommand extends JavaPlugin {
-    // Define variables to store the settings from the config.yml file
+    // Store the settings from the config.yml file in variables
     private List<String> commands;
     private int minInterval, maxInterval;
     private boolean fixedInterval;
     private boolean chatNotification;
     private String chatMessage;
+    private boolean countdownEnabled;
+    private int countdownInterval;
+    // Create a single instance of Random to be used throughout the class
+    private Random random = new Random();
 
     // Called when the plugin is enabled
     @Override
@@ -31,14 +36,22 @@ public class RandomCommand extends JavaPlugin {
         fixedInterval = getConfig().getBoolean("fixedInterval");
         chatNotification = getConfig().getBoolean("chatNotification");
         chatMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("chatMessage"));
+        countdownEnabled = getConfig().getBoolean("countdownEnabled");
+        countdownInterval = getConfig().getInt("countdownInterval");
 
         // Start the task to execute commands
         runCommandTask();
     }
 
+    // Method to run the command task
     private void runCommandTask() {
         // Calculate the interval until the next command execution
-        int interval = fixedInterval ? minInterval : new Random().nextInt(maxInterval - minInterval + 1) + minInterval;
+        int interval = fixedInterval ? minInterval : random.nextInt(maxInterval - minInterval + 1) + minInterval;
+
+        // Start the countdown if enabled
+        if (countdownEnabled) {
+            startCountdown(interval);
+        }
 
         // Schedule the new task
         new BukkitRunnable() {
@@ -47,10 +60,10 @@ public class RandomCommand extends JavaPlugin {
                 // Only execute a command if there are online players
                 if (Bukkit.getOnlinePlayers().size() > 0) {
                     // Choose a random command from the list
-                    String command = commands.get(new Random().nextInt(commands.size()));
+                    String command = commands.get(random.nextInt(commands.size()));
 
                     // Choose a random online player
-                    Player player = (Player) Bukkit.getOnlinePlayers().toArray()[new Random().nextInt(Bukkit.getOnlinePlayers().size())];
+                    Player player = (Player) Bukkit.getOnlinePlayers().toArray()[random.nextInt(Bukkit.getOnlinePlayers().size())];
 
                     // Execute the command
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command.replace("<player>", player.getName()));
@@ -67,7 +80,26 @@ public class RandomCommand extends JavaPlugin {
                 // Schedule the next command execution
                 runCommandTask();
             }
-        }.runTaskLater(this, (long) interval * 60 * 20); // The interval is multiplied by 60*20 to convert minutes to Minecraft ticks (20 ticks = 1 second)
+        }.runTaskLater(this, (long) interval * 60 * 20); // Convert minutes to Minecraft ticks (20 ticks = 1 second)
+    }
+
+    // Method to start the countdown
+    private void startCountdown(int interval) {
+        new BukkitRunnable() {
+            int countdownTime = interval;
+
+            @Override
+            public void run() {
+                // Broadcast a countdown message at intervals defined in the config, and for the last 5 minutes
+                if (countdownTime % countdownInterval == 0 || countdownTime <= 5) {
+                    Bukkit.broadcastMessage(ChatColor.GREEN + "Next command will be executed in " + countdownTime + " minutes.");
+                }
+
+                // Cancel the countdown task when the time is up
+                if (countdownTime-- <= 0) {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(this, 0L, 60 * 20); // Convert minutes to Minecraft ticks (20 ticks = 1 second)
     }
 }
-
